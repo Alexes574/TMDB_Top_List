833adb11f34422c35ccbf967d79d4ac79cd884d7
Alexes574 <Alexes574@gmail.com>
2022-01-21 14:18:22 +0200

    poster added

--- courseProject/UserInterface/MediaCollectionViewCell.swift
+++ courseProject/UserInterface/MediaCollectionViewCell.swift
@@ -2,27 +2,32 @@
 //  MediaCollectionViewCell.swift
 //  courseProject
 //
-//  Created by Alexey Soloviov on 19.01.2022.
+//  Created by Alexey Soloviov on 20.01.2022.
 //
 
 import UIKit
 import SDWebImage
 
 class MediaCollectionViewCell: UICollectionViewCell {
-    @IBOutlet weak var posterImageView: UIImageView!
-    @IBOutlet weak var titleLable: UILabel!
     
-    var _movie : Movie? = nil
+    @IBOutlet weak var titleLable: UILabel!
+    @IBOutlet weak var posterImageView: UIImageView!
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        
     }
     
-    func configure(movie: Movie){
-        titleLable.text = movie.original_title
-        var imageUrl = URL(string: movie.backdrop_path!)
-        posterImageView.sd_setImage(with: imageUrl, completed: nil)
-    }
-    
+        func configure(movie: Movie){
+            
+            titleLable.text = movie.title
+            
+            guard let imagePath = movie.poster_path else { return }
+            let urlString = "https://image.tmdb.org/t/p/w200" + imagePath
+            let imageUrl = URL(string: urlString)
+            posterImageView.sd_setImage(with: imageUrl, completed: nil)
+        }
+
 }
+
+
+//
--- courseProject/ViewController.swift
+++ courseProject/ViewController.swift
@@ -3,20 +3,23 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var sigmentController: UISegmentedControl!
+    @IBOutlet weak var collectionView: UICollectionView!
     
-    @IBOutlet weak var moviescollectionView: UICollectionView!
     var data: Data?
     var moviesList: [Movie] = []
     var serialsList: [Serial] = []
     
     let networkManager = NetworkManager.shared
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let cellNib = UINib(nibName: "MediaCollectionViewCell", bundle: nil)
+        collectionView.register(cellNib, forCellWithReuseIdentifier: "MediaCollectionViewCell")
+        
         let movies = networkManager.loadMoviesList(completionBlock: { [weak self] movies in
             self?.moviesList = movies
-            self?.moviescollectionView.reloadData()
-            
+            self?.collectionView.reloadData()
         })
         
     }
@@ -37,14 +40,29 @@ extension ViewController: UICollectionViewDataSource{
     }
 }
 
+extension ViewController: UICollectionViewDelegate{}
 
 extension ViewController: UICollectionViewDelegateFlowLayout{
+
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         
-        let flowayout = collectionViewLayout as? UICollectionViewFlowLayout
-        let space: CGFloat = (flowayout?.minimumInteritemSpacing ?? 0.0) + (flowayout?.sectionInset.left ?? 0.0) + (flowayout?.sectionInset.right ?? 0.0)
-        let size:CGFloat = (collectionView.frame.size.width - space) / 2.0
-        return CGSize(width: 190, height: 250)
+        let itemsPerRow: CGFloat = 2
+        let paddingWidth = 10 * (itemsPerRow + 1)
+        let availableWith = collectionView.frame.width - paddingWidth
+        let widthPerItem = availableWith / itemsPerRow
+        
+        return CGSize(width: widthPerItem, height: 300)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 10
     }
 }
-
